c9a28672ea38fa7012b8eb9eecb2338dc3ee5425
Peter Lee <peter.lee@tufts.edu>
2015-10-19 13:18:24 -0400

    more json parsing

--- messages/lab.js
+++ messages/lab.js
@@ -1,8 +1,11 @@
 function parse() {
     var messages_json = new XMLHttpRequest();
     messages_json.onload = function() {
-        var jsondata = JSON.parse(messages_json.responseText)
-        console.log(jsondata);
+        var parsed_mes = JSON.parse(messages_json.responseText)
+        messages = document.getElementById("messages");
+        for (count = 0; count < parsed_mes.length; count++) {
+            messages.innerHTML += parsed_mes[count]["content"] + " "
+                                  parsed_mes[count]["username"] + "</br>"
     };
     messages_json.open("GET", "data.json"); 
     messages_json.send();
