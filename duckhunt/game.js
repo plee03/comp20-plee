b8445fcc00a8db775e92e4613aaf9e6099c4f781
Peter Lee <peter.lee@tufts.edu>
2015-10-19 12:27:50 -0400

    Starting lab 6

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,22 @@
-// Replace this with your code...
+/* game.js
+ * By Peter Lee
+ * 
+ * Draws Duck Hunt onto a canvas
+ */
+function init() {
+var bg_canvas = document.getElementById("game_canvas").getContext("2d");
+var bg = new Image();
+bg.onload = function() {
+    bg_canvas.drawImage(bg, 0, 0, 600, 800);
+}
+
+bg.src = "duckhunt-background.gif";
+
+var ducks = new Image();
+ducks.onload = function() {
+    bg_canvas.drawImage(ducks, 0, 110, 40, 40, 300, 200, 100, 100) ;
+    bg_canvas.drawImage(ducks, 40, 150, 40, 40, 400, 100, 100, 100) ;
+
+}
+    ducks.src = "duckhunt_various_sheet.png";
+}
