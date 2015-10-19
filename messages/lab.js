45016270e95d1c8555416cf9d6d399b6c80a6c3a
Peter Lee <peter.lee@tufts.edu>
2015-10-19 12:55:25 -0400

    more xmlhttprequest testing

--- messages/lab.js
+++ messages/lab.js
@@ -1,7 +1,7 @@
 function parse() {
     var messages_json = new XMLHttpRequest();
     messages_json.onload = function() {
-        messages_json.response;
+        console.log(messages_json.response);
     };
     messages_json.open("GET", "data.json"); 
 }
