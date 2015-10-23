16bda506879bf49f0f19e183abe820fc3e535554
Peter Lee <peter.lee@tufts.edu>
2015-10-22 23:17:15 -0400

    used onreadystatechange and updated readme

---
+++ messages/README.txt
@@ -0,0 +1,20 @@
+lab6 README
+by Peter Lee
+October 22, 2015
+
+All aspects of the work have been correctly implemented.
+
+Appoximate hours spent: 2
+
+Is it possible to request the data from a different origin?
+In general, the answer seems to be no, following from the same origin policy. 
+
+From Wikipedia: "XMLHttpRequest requests follow the browser's same-origin 
+policy, and will therefore only succeed if they are made to the host that 
+served the original web page."
+
+However, there seems to be an exception in this case, as the request is to a
+different origin but is still succeeding.
+
+
+
--- messages/lab.js
+++ messages/lab.js
@@ -1,11 +1,13 @@
 function parse() {
     var messages_json = new XMLHttpRequest();
-    messages_json.onload = function() {
-        var parsed_mes = JSON.parse(messages_json.responseText)
-        messages = document.getElementById("messages");
-        for (count = 0; count < parsed_mes.length; count++) {
-            messages.innerHTML += parsed_mes[count]["content"] + " " + 
-                                  parsed_mes[count]["username"] + "</br>"
+    messages_json.onreadystatechange = function() {
+        if (messages_json.readyState == 4 && messages_json.status == 200 ) {
+            var parsed_mes = JSON.parse(messages_json.responseText)
+            messages = document.getElementById("messages");
+            for (count = 0; count < parsed_mes.length; count++) {
+                messages.innerHTML += parsed_mes[count]["content"] + " " + 
+                                      parsed_mes[count]["username"] + "</br>"
+            }
         }
     };
     messages_json.open("GET", "http://messagehub.herokuapp.com/messages.json"); 
